c4299ac0c848568d9694a2c02a03b3f08d949fac
Marko Boskovic <boskovic.marko@gmail.com>
2026-02-02 13:31:10 +0100

    Add list for serialization test

---
+++ src/test/java/org/example/topprogramingstoriesdashboard/scraper/DeserializationTest.java
@@ -0,0 +1,26 @@
+package org.example.topprogramingstoriesdashboard.scraper;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class DeserializationTest {
+
+    @Disabled
+    @Test
+    public void testTopStoriesDeserialization(){
+
+    }
+
+    @Disabled
+    @Test
+    public void testNewStoriesDeserialization(){
+
+    }
+
+    @Disabled
+    @Test
+    public void testItemDeserialization(){
+
+    }
+
+}
