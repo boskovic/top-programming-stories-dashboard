24915cfb6ba824e003340d9e04002598261fdceb
Marko Boskovic <boskovic.marko@gmail.com>
2026-02-05 18:52:56 +0100

    Fix indentation for item

--- src/main/java/org/example/topprogramingstoriesdashboard/scraper/ItemDto.java
+++ src/main/java/org/example/topprogramingstoriesdashboard/scraper/ItemDto.java
@@ -3,19 +3,19 @@ package org.example.topprogramingstoriesdashboard.scraper;
 import java.util.List;
 
 public record ItemDto(
-        Long id,
-        String by,
-        Integer descendants,
-        List<Long> kids,
-        Integer score,
-        Long time,
-        String title,
-        ItemTypeDto type,
-        String url,
-        Boolean deleted,
-        String text,
-        Boolean dead,
-        Long parent,
-        Long poll,
-        List<Long> parts
+    Long id,
+    String by,
+    Integer descendants,
+    List<Long> kids,
+    Integer score,
+    Long time,
+    String title,
+    ItemTypeDto type,
+    String url,
+    Boolean deleted,
+    String text,
+    Boolean dead,
+    Long parent,
+    Long poll,
+    List<Long> parts
 ) { }
