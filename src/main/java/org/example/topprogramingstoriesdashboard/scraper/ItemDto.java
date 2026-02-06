ffd94fa6ed88ca3b2baa1b294629b8d5e7ba5fba
Marko Boskovic <boskovic.marko@gmail.com>
2026-02-06 18:51:45 +0100

    Add builder to the ItemDto

--- src/main/java/org/example/topprogramingstoriesdashboard/scraper/ItemDto.java
+++ src/main/java/org/example/topprogramingstoriesdashboard/scraper/ItemDto.java
@@ -3,19 +3,139 @@ package org.example.topprogramingstoriesdashboard.scraper;
 import java.util.Set;
 
 public record ItemDto(
-    Long id,
-    String by,
-    Integer descendants,
-    Set<Long> kids,
-    Integer score,
-    Long time,
-    String title,
-    ItemTypeDto type,
-    String url,
-    Boolean deleted,
-    String text,
-    Boolean dead,
-    Long parent,
-    Long poll,
-    Set<Long> parts
-) { }
+        Long id,
+        String by,
+        Integer descendants,
+        Set<Long> kids,
+        Integer score,
+        Long time,
+        String title,
+        ItemTypeDto type,
+        String url,
+        Boolean deleted,
+        String text,
+        Boolean dead,
+        Long parent,
+        Long poll,
+        Set<Long> parts
+) {
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        private Long id;
+        private String by;
+        private Integer descendants;
+        private Set<Long> kids;
+        private Integer score;
+        private Long time;
+        private String title;
+        private ItemTypeDto type;
+        private String url;
+        private Boolean deleted;
+        private String text;
+        private Boolean dead;
+        private Long parent;
+        private Long poll;
+        private Set<Long> parts;
+
+        private Builder() {}
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder by(String by) {
+            this.by = by;
+            return this;
+        }
+
+        public Builder descendants(Integer descendants) {
+            this.descendants = descendants;
+            return this;
+        }
+
+        public Builder kids(Set<Long> kids) {
+            this.kids = kids;
+            return this;
+        }
+
+        public Builder score(Integer score) {
+            this.score = score;
+            return this;
+        }
+
+        public Builder time(Long time) {
+            this.time = time;
+            return this;
+        }
+
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder type(ItemTypeDto type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder url(String url) {
+            this.url = url;
+            return this;
+        }
+
+        public Builder deleted(Boolean deleted) {
+            this.deleted = deleted;
+            return this;
+        }
+
+        public Builder text(String text) {
+            this.text = text;
+            return this;
+        }
+
+        public Builder dead(Boolean dead) {
+            this.dead = dead;
+            return this;
+        }
+
+        public Builder parent(Long parent) {
+            this.parent = parent;
+            return this;
+        }
+
+        public Builder poll(Long poll) {
+            this.poll = poll;
+            return this;
+        }
+
+        public Builder parts(Set<Long> parts) {
+            this.parts = parts;
+            return this;
+        }
+
+        public ItemDto build() {
+            return new ItemDto(
+                    id,
+                    by,
+                    descendants,
+                    kids,
+                    score,
+                    time,
+                    title,
+                    type,
+                    url,
+                    deleted,
+                    text,
+                    dead,
+                    parent,
+                    poll,
+                    parts
+            );
+        }
+    }
+}
