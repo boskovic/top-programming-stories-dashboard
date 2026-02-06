48fb7e46eb664593e8066af751eadc2ced2d4852
Marko Boskovic <boskovic.marko@gmail.com>
2026-02-06 10:36:27 +0100

    Add ItemId Fetcher

---
+++ src/main/java/org/example/topprogramingstoriesdashboard/scraper/ItemIdFetcher.java
@@ -0,0 +1,40 @@
+package org.example.topprogramingstoriesdashboard.scraper;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ItemIdFetcher {
+
+    private final NewStoriesGateway newStoriesGateway;
+    private final TopStoriesGateway topStoriesGateway;
+
+    public ItemIdFetcher(NewStoriesGateway newStoriesGateway, TopStoriesGateway topStoriesGateway) {
+        this.newStoriesGateway = newStoriesGateway;
+        this.topStoriesGateway = topStoriesGateway;
+    }
+
+    public Object fetchItemIds() {
+
+        var newStories = newStoriesGateway.getNewStories();
+        var topStories = topStoriesGateway.getTopStories();
+
+        var allIds = Stream.concat(newStories.stream(), topStories.stream()).collect(Collectors.toSet());
+
+        return allIds.stream()
+                .collect(Collectors.toMap(
+                        id -> id,
+                        id -> getRankingsForItem(topStories, newStories, id)));
+
+    }
+
+    private Set<Ranking> getRankingsForItem(Set<Long> topStories, Set<Long> newStories, long itemId) {
+        if (topStories.contains(itemId) && newStories.contains(itemId)) {
+            return Set.of(Ranking.TOP, Ranking.NEW);
+        } else if (topStories.contains(itemId)) {
+            return Set.of(Ranking.TOP);
+        } else {
+            return Set.of(Ranking.NEW);
+        }
+    }
+}
---
+++ src/test/java/org/example/topprogramingstoriesdashboard/scraper/TestItemIdFetcher.java
@@ -0,0 +1,33 @@
+package org.example.topprogramingstoriesdashboard.scraper;
+
+import com.google.common.collect.Sets;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TestItemIdFetcher {
+
+    @Test
+    public void itemIdFetcherFetchesTheItemIdsFromDifferentRankings() {
+        var newStoriesGatewayMock = mock(NewStoriesGateway.class);
+        var newStories = Sets.newHashSet(12345L, 12346L);
+        when(newStoriesGatewayMock.getNewStories()).thenReturn(newStories);
+        var topStoriesGatewayMock = mock(TopStoriesGateway.class);
+        var topStories = Sets.newHashSet(12345L, 12347L);
+        when(topStoriesGatewayMock.getTopStories()).thenReturn(topStories);
+        var expectedResult = Map.of(
+        12345L, Sets.newHashSet(Ranking.TOP, Ranking.NEW),
+        12346L, Sets.newHashSet(Ranking.NEW),
+        12347L, Sets.newHashSet(Ranking.TOP)
+        );
+        var itemIdFetcher = new ItemIdFetcher(newStoriesGatewayMock, topStoriesGatewayMock);
+
+        var result = itemIdFetcher.fetchItemIds();
+
+        assertThat(expectedResult).isEqualTo(result);
+    }
+}
