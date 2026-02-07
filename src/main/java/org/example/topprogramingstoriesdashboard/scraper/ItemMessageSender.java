070c6256739fe4854981866d0c5fa628f768f78c
Marko Boskovic <boskovic.marko@gmail.com>
2026-02-07 18:29:42 +0100

    Add message sender with testcontainer kafka

---
+++ src/main/java/org/example/topprogramingstoriesdashboard/scraper/ItemMessageSender.java
@@ -0,0 +1,18 @@
+package org.example.topprogramingstoriesdashboard.scraper;
+
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class ItemMessageSender {
+
+    public static final String KAFKA_TOPIC = "hn-stories";
+
+    private final KafkaTemplate<String, ItemMessage> kafkaTemplate;
+
+    public ItemMessageSender(KafkaTemplate<String, ItemMessage> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void send(ItemMessage itemMessage) {
+        kafkaTemplate.send(KAFKA_TOPIC, itemMessage);
+    }
+}
---
+++ src/test/java/org/example/topprogramingstoriesdashboard/scraper/ItemMessageSenderTest.java
@@ -0,0 +1,43 @@
+package org.example.topprogramingstoriesdashboard.scraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.Set;
+
+@SpringBootTest
+public class ItemMessageSenderTest {
+    // Start Kafka container
+    static KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("apache/kafka"));
+
+    static {
+        kafkaContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void kafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
+    }
+
+    @Autowired
+    private KafkaTemplate<String, ItemMessage> kafkaTemplate;
+
+    @Test
+    void testSendReceive() throws InterruptedException {
+        var ID = 10L;
+        var rankings = Set.of(Ranking.NEW, Ranking.TOP);
+        var itemMessage = ItemMessage.builder().id(ID).rankings(rankings).build();
+        var sus = new ItemMessageSender(kafkaTemplate);
+
+        sus.send(itemMessage);
+        // Usually you wait a bit or use a KafkaListener to verify
+        Thread.sleep(2000); // simple wait for demo purposes
+        System.out.println("Message sent to Kafka container!");
+    }
+}
