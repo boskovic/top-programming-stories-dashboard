a9937b45d171cae0bd1ca1d2415199a2d4f749f1
Marko Boskovic <boskovic.marko@gmail.com>
2026-03-06 19:26:33 +0100

    Add and test topics repository

---
+++ src/main/java/org/example/topprogramingstoriesdashboard/backend/service/TopicsRepository.java
@@ -0,0 +1,24 @@
+package org.example.topprogramingstoriesdashboard.backend.service;
+
+import org.example.topprogramingstoriesdashboard.scraper.messaging.ItemMessage;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+public class TopicsRepository {
+
+    private final Set<ItemMessage> items = new HashSet<>();
+
+    public void save(ItemMessage itemMessage) {
+        items.add(itemMessage);
+    }
+
+    public Optional<ItemMessage> find(Long id){
+        return items.stream()
+                .filter(itemMessage -> Objects.equals(itemMessage.id(), id))
+                .findAny();
+    }
+
+}
---
+++ src/test/java/org/example/topprogramingstoriesdashboard/backend/service/TopicsRepositoryTest.java
@@ -0,0 +1,36 @@
+package org.example.topprogramingstoriesdashboard.backend.service;
+
+import org.example.topprogramingstoriesdashboard.scraper.messaging.ItemMessage;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TopicsRepositoryTest {
+
+    @Test
+    public void findSavedInRepository() {
+        var ID1 = 1L;
+        var itemMessage = ItemMessage.builder().id(ID1).build();
+        var sus = new TopicsRepository();
+        sus.save(itemMessage);
+
+        var result = sus.find(ID1);
+
+        assertSoftly(softAssertions -> {
+                softAssertions.assertThat(result.isPresent()).isTrue();
+                softAssertions.assertThat(itemMessage).isEqualTo(result.get());
+            }
+        );
+    }
+
+    @Test
+    public void dontFindAnythingIfNotSaved() {
+        var ID = 1L;
+        var sus = new TopicsRepository();
+
+        var result = sus.find(ID);
+
+        assertFalse(result.isPresent());
+    }
+}
